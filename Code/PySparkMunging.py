3f1a9d2397be212064543fc4739f7934d0b99641
FNeffati <nebli55mohamed@gmail.com>
2023-04-28 09:54:25 -0400

    fixed path to csv

--- Code/PySparkMunging.py
+++ Code/PySparkMunging.py
@@ -7,7 +7,7 @@ def getFrame(path):
     return pd.read_csv(path)
 
 
-url = "/home/fneffati/DataSets/dcp2data.csv"
+url = "/home/fneffati/DataSets/dcp2data/fbpac-ads-en-US.csv"
 dataframe = getFrame(url)
 sc = pyspark.SparkContext("local[*]", "Test Context")
 rdd = sc.textFile(dataframe)
