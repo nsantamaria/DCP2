cfe3445a625a124bd0d1484cca63809b1d23c45e
FNeffati <nebli55mohamed@gmail.com>
2023-05-01 09:41:03 -0400

    extracting targets raw

--- Code/PySparkMunging.py
+++ Code/PySparkMunging.py
@@ -11,14 +11,22 @@ def splitter(row):
     return row.split(',')
 
 
+def target_extractor(split_row):
+    if split_row[15] != "[]":
+        return split_row[15]
+    else:
+        return "empty"
+
+
 file_path = "/home/fneffati/DataSets/dcp2data/fbpac-ads-en-US.csv"
 
 sc = pyspark.SparkContext("local[*]", "Test Context")
 rdd = sc.textFile(file_path)
 
 split_rows = rdd.map(splitter)
+raw_targets = split_rows.map(target_extractor)
 
-for row in split_rows.take(5):
+for row in raw_targets.take(5):
     print(row[15])
 
 
