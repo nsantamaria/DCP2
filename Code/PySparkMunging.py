9d6563101d34b1292ce02ee33792e77cb12ba94d
FNeffati <nebli55mohamed@gmail.com>
2023-05-02 09:47:30 -0400

    printing 5 top rows again

--- Code/PySparkMunging.py
+++ Code/PySparkMunging.py
@@ -7,10 +7,8 @@ from pyspark.sql import SparkSession
 def getFrame(path):
     return pd.read_csv(path)
 
-
 def splitter(row):
     return row.split(',')
-
 
 def target_extractor(split_row):
     return split_row[15]
@@ -28,6 +26,10 @@ raw_targets = split_rows.map(target_extractor)"""
 spark = SparkSession.builder.appName('SparkExamples').getOrCreate()
 df = spark.read.csv(file_path)
 
+split_rows = df.map(splitter)
+raw_targets = split_rows.map(target_extractor)
+
 # View the dataframe
-df.show(5)
+for row in raw_targets.take(5):
+    print(row)
 
