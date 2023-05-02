64cf7c552eb0e1238f6b1360e164ab073478410c
FNeffati <nebli55mohamed@gmail.com>
2023-05-02 08:56:31 -0400

    Trying SparkSession instead of SparkContext

--- Code/PySparkMunging.py
+++ Code/PySparkMunging.py
@@ -20,13 +20,16 @@ def target_extractor(split_row):
 
 file_path = "/home/fneffati/DataSets/propublica_1000.csv"
 
-sc = pyspark.SparkContext("local[*]", "Test Context")
-rdd = sc.textFile(file_path)
+"""sc = pyspark.SparkContext("local[*]", "Test Context")
+rdd = sc.textFile(file_path)"""
+
+ss = pyspark.sq1.SparkSession.builder.getOrCreate()
+rdd = ss.read.csv(file_path)
 
 split_rows = rdd.map(splitter)
 raw_targets = split_rows.map(target_extractor)
 
-for row in raw_targets.take(5):
+for row in raw_targets.take(10):
     print(row)
 
 
