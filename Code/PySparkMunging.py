ce349c04d4e637fc4587313ae1b590183564a567
FNeffati <nebli55mohamed@gmail.com>
2023-05-04 08:19:31 -0400

    trying yet another approach

--- Code/PySparkMunging.py
+++ Code/PySparkMunging.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import json
 import pyspark
-# Create a spark session
-from pyspark.sql import SparkSession
+
 
 def getFrame(path):
     return pd.read_csv(path)
 
+
 def splitter(row):
     return row.split(',')
+
 
 def target_extractor(split_row):
     return split_row[15]
@@ -16,20 +17,13 @@ def target_extractor(split_row):
 
 file_path = "/home/fneffati/DataSets/propublica_1000.csv"
 
-"""sc = pyspark.SparkContext("local[*]", "Test Context")
-rdd = sc.textFile(file_path)
-
+sc = pyspark.SparkContext("local[*]", "Test Context")
+rdd = sc.read.csv("/home/rblaha/fb_monolith.csv", header=True, inferSchema=True, multiLine=True, sep=',', escape='"',
+                  ignoreLeadingWhiteSpace=True)
 
 split_rows = rdd.map(splitter)
-raw_targets = split_rows.map(target_extractor)"""
-
-spark = SparkSession.builder.appName('SparkExamples').getOrCreate()
-df = spark.read.csv(file_path)
-
-split_rows = df.map(splitter)
 raw_targets = split_rows.map(target_extractor)
 
 # View the dataframe
 for row in raw_targets.take(5):
     print(row)
-
