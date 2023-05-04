8c2f7eafbd53b8857c9120cb6b117716dff0bcc2
FNeffati <nebli55mohamed@gmail.com>
2023-05-04 10:50:33 -0400

    json formatting rows attempt 1.0

--- Code/PySparkMunging.py
+++ Code/PySparkMunging.py
@@ -13,11 +13,8 @@ def quoter(row):
     return re.sub('""', '"', row)
 
 
-def json_converter(row):
-    return json.loads(row)
-
-
 def col_name_extractor(row):
+    row = json.loads(row)
     return "target" + "_" + row["target"]
 
 
@@ -34,9 +31,6 @@ rdd = df.select("targets").rdd
 rdd2 = rdd.map(lambda x: x[0])
 rdd3 = rdd2.map(quoter)
 
-jsoned_set = rdd3.map(json_converter)
-print(jsoned_set.take(5))
-
-cols = jsoned_set.map(col_name_extractor)
+cols = rdd3.map(col_name_extractor)
 print(cols.take(5))
 
